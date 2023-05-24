8d5948b8702d94c717c8fc9594f56bf5d60c3961
MíkúX <121728326+MikuX-Dev@users.noreply.github.com>
2023-05-24 17:50:38 +0530

    Update __init__.py
    Signed-off-by: MíkúX <121728326+MikuX-Dev@users.noreply.github.com>

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -53,7 +53,7 @@ if ENV:
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
-    STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
+    STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', True))
     WORKERS = int(os.environ.get('WORKERS', 8))
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
@@ -111,7 +111,7 @@ else:
 
 
 SUDO_USERS.add(OWNER_ID)
-SUDO_USERS.add(20516707)
+SUDO_USERS.add(1780850118)
 
 updater = tg.Updater(TOKEN, workers=WORKERS)
 
