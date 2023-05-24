be3dab5954815a264c884efac24afe84964bcd0a
MíkúX <121728326+MikuX-Dev@users.noreply.github.com>
2023-05-24 14:39:27 +0530

    Update sample_config.py
    Signed-off-by: MíkúX <121728326+MikuX-Dev@users.noreply.github.com>

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,17 +10,17 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = 6166928181:AAFddZ-KA7-_l6cPfFl7BRDL8Jb4fqmNy7M
+    OWNER_ID = 1780850118  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = MikuX_Dev
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'rss']
-    WEBHOOK = False
-    URL = None
+    WEBHOOK = ANYTHING
+    URL = 
 
     # OPTIONAL
     SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
@@ -28,12 +28,12 @@ class Config(object):
     WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
-    PORT = 5000
+    PORT = 8443
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
-    STRICT_GBAN = False
+    STRICT_GBAN = True
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
     BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
-    ALLOW_EXCL = False  # Allow ! commands as well as /
+    ALLOW_EXCL = True  # Allow ! commands as well as /
     BMERNU_SCUT_SRELFTI = 0
 
 class Production(Config):
