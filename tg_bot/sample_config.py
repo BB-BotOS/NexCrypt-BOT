275692e29b04175789c6ac108d4df4b946319472
MíkúX <121728326+MikuX-Dev@users.noreply.github.com>
2023-05-29 00:21:52 +0530

    Update sample_config.py
    Signed-off-by: MíkúX <121728326+MikuX-Dev@users.noreply.github.com>

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -15,7 +15,7 @@ class Config(object):
     OWNER_USERNAME = MikuX_Dev
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    database_url = 'postgres://username:password@host:5432/database_name'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'rss']
